58903f490bb7dd8a946440d1adb0eea6530f1ea0
Luis Sosa <94214924+LuisJSosa@users.noreply.github.com>
2024-06-24 12:46:47 -0500

    Update hellojava.java

--- hellojava.java
+++ hellojava.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         int x = 0;
-        if (x < 11) {
+        if (x < 12) {
             while (x < 10) {
                 System.out.println("hello world");
                 x++;
