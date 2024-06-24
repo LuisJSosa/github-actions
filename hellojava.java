361bd654c0bab7bfd5d233c7d27f45e7eb91b161
Luis Sosa <94214924+LuisJSosa@users.noreply.github.com>
2024-06-24 09:49:14 -0500

    Create hellojava.java

---
+++ hellojava.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 0;
+        if (x < 11) {
+            while (x < 10) {
+                System.out.println("hello world");
+                x++;
+            }
+        }
+    }
+}
